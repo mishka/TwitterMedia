7cd959e1d65c6cd5e672cb6daf4ed841dfe98afd
浪人 <mighxi@yahoo.com>
2021-07-14 19:28:05 +0300

    v0.5
    Added a banning system for some users with abusive behavior.

--- twittermd_Bot.py
+++ twittermd_Bot.py
@@ -3,12 +3,20 @@ from datetime import datetime
 
 from TwitterMedia import TwitterMedia
 
+import sqlite3 as sql
+from sqlite3.dbapi2 import IntegrityError
+
 from telegram import ParseMode
 from telegram.ext import Updater, MessageHandler, CommandHandler
 
 
 Pattern = r'(https?://)?(mobile.)?twitter.com/.+?/status/\d+'
-Downloader = TwitterMedia()
+Downloader = TwitterMedia(use_print = True)
+
+
+con = sql.connect('banned.db', isolation_level = None, check_same_thread = False)
+c = con.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS punished (id INTEGER NOT NULL PRIMARY KEY);')
 
 
 def log(text):
@@ -18,22 +26,51 @@ def log(text):
 def start(update, context):
     context.bot.send_message(
         chat_id = update.effective_chat.id,
-        text = "Hi, send a twitter status url that contains a video/gif to use this bot.\nIf you face any issues please message @ulfsjar\n(please say hi im so lonely)"
+        text = "Hi, send a twitter status url that contains a video/gif to use this bot.\nIf you face any issues please message @ulfsjar"
     )
 
 
+def ban(update, context):
+    if int(update.message.chat_id) != 'enter your own user id here.':
+        return
+    else:
+        try:
+            prisoner = context.args[0]
+            log(f'Banning {prisoner}')
+            
+            c.execute('INSERT INTO punished VALUES (?)', (prisoner,))
+            context.bot.send_message(
+                chat_id = update.message.chat_id,
+                parse_mode = ParseMode.MARKDOWN,
+                text = f'Banned [{prisoner}](tg://user?id={prisoner})'
+            )
+        except IntegrityError:
+            log('The id is probably already in the database, ignoring.')
+    
+
 def processor(update, context):
-    text = update.message.text
+    text = update.message.text    
     chat_id = update.message.chat_id
     name = update.message.chat.first_name
     username = update.message.chat.username
     message_id = update.message.message_id
 
+    c.execute('SELECT id FROM punished WHERE id=?', (chat_id,))
+    exists = c.fetchall()
+    
+    if exists:
+        log(f'{chat_id} is banned, skipping..')
+        return
+        
+    if not text:
+        log(f"@{username} | {name} | {chat_id}\nRequest doesn't contain text, skipping...")
+        return
+
     log(f'Request: {username} | {name} | {text}')
-    
+
     result = re.search(Pattern, text)
-    
-    if not result:   
+
+    if not result:
         context.bot.send_message(
             chat_id = chat_id,
             reply_to_message_id = message_id,
@@ -41,7 +78,7 @@ def processor(update, context):
             parse_mode = ParseMode.MARKDOWN
         )
         return
-
+    
     try:
         tweet = Downloader.fetch_media(result.group(0))
     except:
@@ -52,7 +89,7 @@ def processor(update, context):
             parse_mode = ParseMode.MARKDOWN
         )
         return
-    
+
     if tweet is not None:
         try:
             context.bot.send_video(
@@ -75,7 +112,6 @@ def processor(update, context):
             parse_mode = ParseMode.MARKDOWN
         )
 
-
 def main():
     updater = Updater(
         token = '',
@@ -84,6 +120,7 @@ def main():
     )
 
     dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('ban', ban, pass_args = True))
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(MessageHandler(None, processor))
 
@@ -92,4 +129,5 @@ def main():
 
 
 if __name__ == '__main__':
+    log('ONLINE')
     main()
